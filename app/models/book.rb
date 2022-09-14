b0cb0b613638a6c98b6d48e385255e682d6c8906
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-09-14 23:43:17 +0000

    [応用課題3]コメント機能実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 end
